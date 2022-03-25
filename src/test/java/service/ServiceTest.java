99c4b4d73d48084ffd4a44f89deb074735036064
qqqqq <fluturasu19@gmail.com>
2022-03-25 09:39:10 +0200

    bruh
    bruh

---
+++ src/test/java/service/ServiceTest.java
@@ -0,0 +1,63 @@
+package service;
+
+import domain.Nota;
+import domain.Tema;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repository.*;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    private StudentValidator studentValidator = new StudentValidator();
+    private Validator<Tema> temaValidator = new TemaValidator();
+    private Validator<Nota> notaValidator = new NotaValidator();
+
+
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+    TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+    NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+    private Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+    @BeforeEach
+    void setUp() {
+        }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void saveTema() {
+
+    }
+
+    @Test
+    void saveStudentGrupa() {
+        int grupa = -222;
+        int returnValue = service.saveStudent("19", "bruhtest", grupa);
+        assertEquals(returnValue, 1);
+    }
+
+    @Test
+    void saveStudentGrupaWithNegativeGrupa() {
+        int grupa = -1;
+        int returnValue = service.saveStudent("19", "bruhtest", grupa);
+        assertEquals(returnValue, 1);
+    }
+
+
+
+    @Test
+    void saveStudentId() {
+        String id = "-1";
+        service.saveStudent(id, "bruhtest", 222);
+    }
+}
