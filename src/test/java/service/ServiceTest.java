b067110ba24e4aad8451d3bba8c79ff7d56cdc21
qqqqq <fluturasu19@gmail.com>
2022-03-27 13:53:34 +0300

    bruh3

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import domain.Nota;
+import domain.Student;
 import domain.Tema;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,17 @@ import org.junit.jupiter.api.Test;
 import repository.*;
 import validation.*;
 import validation.ValidationException;
+
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,30 +30,55 @@ class ServiceTest {
     private Validator<Nota> notaValidator = new NotaValidator();
 
 
-    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-    TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-    NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+    StudentXMLRepository fileRepository1;
+    TemaXMLRepository fileRepository2;
+    NotaXMLRepository fileRepository3;
 
-    private Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    private Service service;
 
     @BeforeEach
-    void setUp() {
-        }
+    void setUp() throws IOException {
+        fileRepository1 = new StudentXMLRepository(studentValidator, "studenti_test.xml");
+        fileRepository2 = new TemaXMLRepository(temaValidator, "teme_test.xml");
+        fileRepository3 = new NotaXMLRepository(notaValidator, "note_test.xml");
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws IOException {
+        deleteAllStudents();
+        deleteAllTema();
+        deleteAllNota();
     }
 
-    @Test
-    void saveTema() {
-
+    private void deleteAllStudents() {
+        List<Student> studentCopies = StreamSupport.stream(fileRepository1.findAll().spliterator(), false).collect(Collectors.toList());
+        for (Student student: studentCopies) {
+            fileRepository1.delete(student.getID());
+        }
     }
+
+    private void deleteAllTema() {
+        List<Tema> temaCopies = StreamSupport.stream(fileRepository2.findAll().spliterator(), false).collect(Collectors.toList());
+        for (Tema tema: temaCopies) {
+            fileRepository2.delete(tema.getID());
+        }
+    }
+
+    private void deleteAllNota() {
+        List<Nota> temaCopies = StreamSupport.stream(fileRepository3.findAll().spliterator(), false).collect(Collectors.toList());
+        for (Nota nota: temaCopies) {
+            fileRepository3.delete(nota.getID());
+        }
+    }
+
 
     @Test
     void saveStudentGrupa() {
         int grupa = -222;
         int returnValue = service.saveStudent("19", "bruhtest", grupa);
         assertEquals(returnValue, 1);
+        assertNull(fileRepository1.findOne("19"));
     }
 
     @Test
@@ -49,6 +86,7 @@ class ServiceTest {
         int grupa = -1;
         int returnValue = service.saveStudent("19", "bruhtest", grupa);
         assertEquals(returnValue, 1);
+        assertNull(fileRepository1.findOne("19"));
     }
 
     @Test
@@ -58,6 +96,7 @@ class ServiceTest {
         int grupa = 933;
         int returnValue = service.saveStudent(id, nume, grupa);
         assertEquals(returnValue, 1);
+        assertNull(fileRepository1.findOne(id));
     }
 
     @Test
@@ -67,11 +106,25 @@ class ServiceTest {
         int grupa = 499;
         int returnValue = service.saveStudent(id, nume, grupa);
         assertEquals(returnValue, 1);
+        assertNull(fileRepository1.findOne(id));
+    }
+
+
+    @Test
+    void saveStudentWithMinBoundaryGroup() {
+        String id = "999999";
+        String nume = "2";
+        int grupa = 111;
+        int returnValue = service.saveStudent(id,nume,grupa);
+        assertEquals(returnValue, 1);
+        assertNotNull(fileRepository1.findOne(id));
     }
 
     @Test
     void saveStudentId() {
         String id = "-1";
-        service.saveStudent(id, "bruhtest", 222);
+        int returnValue = service.saveStudent(id, "bruhtest", 222);
+        assertEquals(returnValue, 1);
+        assertNotNull(fileRepository1.findOne(id));
     }
 }
